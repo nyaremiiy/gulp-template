4f1cc3c40eba6037f821ff6fcbfabfb8ae978181
Nazar Yaremii <n.yaremiy791@gmail.com>
2023-02-15 17:47:12 +0200

    Created html file and wrote the basic paths

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,17 @@ const gulp = require('gulp');
 const scss = require('scss');
 const del = require('del');
 
+const path = {
+  styles : {
+    src: 'src/styles/**/*.scss',
+    dest: 'dist/css/'
+  },
+  scripts : {
+    src: 'src/scripts/**/*.js',
+    dest: 'dist/js/'
+  }
+}
+
 function clean() {
   return del(['dist']);
 }
